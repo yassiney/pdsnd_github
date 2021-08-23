4a212bb376b4af1625493a218763c1cc9a378d6b
yassine <bouafia.yassine.sim@gmail.com>
2021-08-23 19:34:30 +0200

    refactor: Update the print statement in the end of the code

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -349,7 +349,7 @@ def main():
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
-            print("exit ...")
+            print("exit ... \n Good bye  \U0001F603")
             break
 
 
